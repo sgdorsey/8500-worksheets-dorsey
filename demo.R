6ac5f59b5e2558d698d1c62320cb6214e2f4220b
Sharon Dorsey <sldorse@clemson.edu>
2026-02-09 17:08:56 -0500

    fixed demo 2/9

--- demo.R
+++ demo.R
@@ -1,7 +1,17 @@
-nrow(BostonWomenVoters)
-library(DigitalMethodsData)
-numrows <- 10702
 
-numrows>=
-BostonWomenVoters>=100
-numrows>100
+
+library(DigitalMethodsData)
+data(BostonWomenVoters)
+
+for (i in 1:nrow(BostonWomenVoters)){
+if (BostonWomenVoters$Occupation[i] == "Housewife") {
+  print(paste("BostonWomenVoters$Name[i])","is a housewife"))
+} else if (BostonWomenVoters$Occupation[i] == "Stenographer") {
+  print("this woman is a stenographer")
+} else {
+  print("this woman is neither")
+}
+}
+
+
+
