add2cfca66dc744b54e682f828b1a37e39a03d09
Sharon Dorsey <sldorse@clemson.edu>
2026-02-11 17:08:51 -0500

    demos from class 2 11

--- demo.R
+++ demo.R
@@ -13,5 +13,23 @@ if (BostonWomenVoters$Occupation[i] == "Housewife") {
 }
 }
 
+journals <- data.frame(
+  title = c("AHR", 
+            "JAH",
+            "WMQ",
+            "AHR",
+            "JSH"),
+  month = c("January", "March", "April", "July", "October"),
+  year = c(1920, 1920, 1920, 1921, 1921)
+)
+
+#AHR-January-1920.txt "A profound historical source"
+
+writeLines("A profound historical source", "AHR-January-1920.txt")
+for (i in 1:length(journals$title)){
+  writeLines("A profound historical source", paste(journals$title[i], journals$month[i], journals$year[i], ".txt", sep="-"))
+}
 
 
+
+paste(journals$title[1], journals$month[1], journals$year[1], ".txt", sep="-")
